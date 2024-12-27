5df8460d2ac32eeee838ebb7fc0d8185241d4507
Titouan Le Gourrierec <titouanlegourrierec@icloud.com>
2024-12-27 16:04:37 +0100

    feat: Add function to generate tile coordinates from bounding box and corresponding tests

--- elevatr/utils.py
+++ elevatr/utils.py
@@ -1,5 +1,8 @@
 import math
+from itertools import product
 from typing import Tuple
+
+import pandas as pd
 
 
 def _lonlat_to_tilenum(lon_deg: float, lat_deg: float, zoom: int) -> Tuple[int, int]:
@@ -55,3 +58,87 @@ def _lonlat_to_tilenum(lon_deg: float, lat_deg: float, zoom: int) -> Tuple[int, 
     ytile = max(0, min(n_tiles - 1, int(y * n_tiles)))
 
     return xtile, ytile
+
+
+def _get_tile_xy(bbx: dict, zoom: int) -> pd.DataFrame:
+    """Generate a DataFrame of tile coordinates within a bounding box at a specific zoom level.
+
+    Parameters
+    ----------
+    bbx : dict
+        A dictionary representing the bounding box with keys 'xmin', 'xmax', 'ymin', and 'ymax'.
+        - 'xmin' : float
+            Minimum longitude of the bounding box.
+        - 'xmax' : float
+            Maximum longitude of the bounding box.
+        - 'ymin' : float
+            Minimum latitude of the bounding box.
+        - 'ymax' : float
+            Maximum latitude of the bounding box.
+    zoom : int
+        Zoom level, a non-negative integer where higher values correspond to more detailed tiles.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame containing the tile coordinates within the bounding box at the specified zoom level.
+        The DataFrame has two columns:
+        - 'tile_x' : int
+            The tile number along the x-axis (longitude).
+        - 'tile_y' : int
+            The tile number along the y-axis (latitude).
+
+    Notes
+    -----
+    - The function normalizes the bounding box coordinates to ensure they are within valid ranges.
+    - Tile indices are clamped to valid ranges: `[0, 2**zoom - 1]`.
+    - The function uses the Web Mercator projection to map geographic coordinates onto a 2D plane.
+    - For zoom levels 0 and 1, the function ensures that the tile coordinates are within the valid range for
+    those zoom levels.
+
+    Examples
+    --------
+    >>> bbx = {'xmin': -5.14, 'ymin': 41.33, 'xmax': 9.56, 'ymax': 51.09}
+    >>>_get_tile_xy(bbx, 6)
+       tile_x  tile_y
+    0      31      21
+    1      31      22
+    2      31      23
+    3      32      21
+    4      32      22
+    5      32      23
+    6      33      21
+    7      33      22
+    8      33      23
+    """
+
+    # Normalize bounding box coordinates
+    bbx["xmin"], bbx["xmax"] = sorted([bbx["xmin"], bbx["xmax"]])
+    bbx["ymin"], bbx["ymax"] = sorted([bbx["ymin"], bbx["ymax"]])
+    bbx["xmin"] = bbx["xmin"] - 360 if bbx["xmin"] > 180 else bbx["xmin"]
+    bbx["xmax"] = bbx["xmax"] - 360 if bbx["xmax"] > 180 else bbx["xmax"]
+
+    # Convert to float
+    bbx = {k: float(v) for k, v in bbx.items()}
+
+    # Get tile numbers
+    min_tile = _lonlat_to_tilenum(bbx["xmin"], bbx["ymin"], zoom)
+    max_tile = _lonlat_to_tilenum(bbx["xmax"], bbx["ymax"], zoom)
+    xmin, xmax = min_tile[0], max_tile[0]
+    ymin, ymax = min_tile[1], max_tile[1]
+
+    if ymin > ymax:
+        ymin, ymax = ymax, ymin
+
+    # Generate tile coordinates
+    tiles = pd.DataFrame(
+        product(range(xmin, xmax + 1), range(ymin, ymax + 1)),
+        columns=["tile_x", "tile_y"],
+    )
+
+    if zoom == 1:
+        tiles = tiles[(tiles["tile_x"] < 2) & (tiles["tile_y"] < 2)]
+    elif zoom == 0:
+        tiles = tiles[(tiles["tile_x"] < 1) & (tiles["tile_y"] < 1)]
+
+    return tiles
--- tests/test_elevatr.py
+++ tests/test_elevatr.py
@@ -1,6 +1,6 @@
 import pytest
 
-from elevatr.utils import _lonlat_to_tilenum
+from elevatr.utils import _get_tile_xy, _lonlat_to_tilenum
 
 
 @pytest.mark.parametrize(
@@ -22,3 +22,21 @@ def test_lonlat_to_tilenum(lon_deg, lat_deg, zoom, expected):
     assert (
         result == expected
     ), f"Failed for lon={lon_deg}, lat={lat_deg}, zoom={zoom}. Got: {result}, expected: {expected}"
+
+
+@pytest.mark.parametrize(
+    "bbx, zoom, max_tile_x, max_tile_y",
+    [
+        ({"xmin": -10, "xmax": 10, "ymin": -10, "ymax": 10}, 0, 0, 0),
+        ({"xmin": -10, "xmax": 10, "ymin": -10, "ymax": 10}, 1, 1, 1),
+    ],
+)
+def test_get_tile_xy_zoom_levels(bbx, zoom, max_tile_x, max_tile_y):
+    """Test if the tiles are correctly filtered based on zoom level."""
+    result = _get_tile_xy(bbx, zoom)
+    assert (
+        result["tile_x"] <= max_tile_x
+    ).all(), f"Tile x-coordinates exceed max for zoom {zoom}"
+    assert (
+        result["tile_y"] <= max_tile_y
+    ).all(), f"Tile y-coordinates exceed max for zoom {zoom}"
