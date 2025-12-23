fee8492953e0fec4302cb315c6b124c8e1251b6b
Titouan Le Gourrierec <titouan.le-gourrierec@etu.mines-ales.fr>
2025-12-23 17:51:52 +0900

    chore: migrate to pyproject.toml

--- elevatr/__init__.py
+++ elevatr/__init__.py
@@ -4,9 +4,15 @@ __license__ = "MIT"
 __description__ = "A Python package to simplify downloading and processing elevation data."
 
 
+from importlib.metadata import PackageNotFoundError, version
+
 from . import settings  # noqa F401
-from ._version import __version__  # noqa F401
 from .get_elev_raster import get_elev_raster
 from .raster import Raster  # noqa F401
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
 
 __all__ = ["get_elev_raster", "Raster", "settings"]
